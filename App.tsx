ce5d30980db9bdcbc3df9c7682dbe411dc389dd6
João Trajano <joao.trajanosouza@gmail.com>
2024-08-27 16:41:22 -0300

    feature(statusBar): added StatusBar

--- App.tsx
+++ App.tsx
@@ -1,4 +1,4 @@
-import { Text, View } from "react-native";
+import { StatusBar, Text, View } from "react-native";
 import {
   useFonts,
   Roboto_400Regular,
@@ -8,7 +8,19 @@ import {
 export default function App() {
   const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold });
   return (
-    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+    <View
+      style={{
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+        backgroundColor: "#202024",
+      }}
+    >
+      <StatusBar
+        barStyle="light-content"
+        backgroundColor="transparent"
+        translucent
+      />
       {fontsLoaded ? <Text>Home</Text> : null}
     </View>
   );
