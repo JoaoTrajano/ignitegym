60937e8c3be5b792745195cccefa10cf3c716050
João Trajano <joao.trajanosouza@gmail.com>
2024-08-27 16:39:03 -0300

    feature(font): added fonts

--- App.tsx
+++ App.tsx
@@ -1,9 +1,15 @@
 import { Text, View } from "react-native";
+import {
+  useFonts,
+  Roboto_400Regular,
+  Roboto_700Bold,
+} from "@expo-google-fonts/roboto";
 
 export default function App() {
+  const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold });
   return (
-    <View>
-      <Text>Home</Text>
+    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+      {fontsLoaded ? <Text>Home</Text> : null}
     </View>
   );
 }
