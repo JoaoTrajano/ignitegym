ffc65a7be0a9a86520f0fda69ed890fafeddd0ec
João Trajano <joao.trajanosouza@gmail.com>
2024-08-27 16:44:03 -0300

    feature(assets): added the folder assets

--- App.tsx
+++ App.tsx
@@ -1,12 +1,13 @@
 import { StatusBar, Text, View } from "react-native";
 import {
   useFonts,
-  Roboto_400Regular,
   Roboto_700Bold,
+  Roboto_400Regular,
 } from "@expo-google-fonts/roboto";
 
 export default function App() {
   const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold });
+
   return (
     <View
       style={{
