ffba389126cb14ad597cae0c1329bb3c655d075e
João Trajano <joao.trajanosouza@gmail.com>
2024-08-28 10:10:34 -0300

    feature(component): created input

--- src/screens/SignIn.tsx
+++ src/screens/SignIn.tsx
@@ -1,5 +1,8 @@
-import { VStack, Image } from "@gluestack-ui/themed";
+import { VStack, Image, Center, Text, Heading } from "@gluestack-ui/themed";
+
 import BackgroundImg from "@assets/background.png";
+import Logo from "@assets/logo.svg";
+import { Input } from "@components/Input";
 
 export function SignIn() {
   return (
@@ -12,6 +15,23 @@ export function SignIn() {
         position="absolute"
         alt="Imagem de fundo"
       />
+      <VStack flex={1} px="$10" pb="$16">
+        <Center my="$24">
+          <Logo />
+          <Text color="$gray100" fontSize={"$sm"}>
+            Treine a sua mente e o seu corpo.
+          </Text>
+        </Center>
+        <Center gap="$2" w="$full">
+          <Text color="$gray100">Acesse a sua conta.</Text>
+          <Input
+            placeholder="E-mail"
+            keyboardType="email-address"
+            autoCapitalize="none"
+          />
+          <Input placeholder="Senha" secureTextEntry />
+        </Center>
+      </VStack>
     </VStack>
   );
 }
